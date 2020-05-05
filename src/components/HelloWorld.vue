70e83f8231c702e226b2f7b21c8ac17dc35a23c1
Abhinash <avinash.sharmaz16.1995@gmail.com>
2020-05-05 18:50:33 +0530

    'initial'

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,530 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="overview-project">
+    <div class="top-container col-md-12 m-0 p-0">
+      <div class="navigation-container m-0 p-0">
+        <navbar/>
+      </div>
+      <!-- <div class="col-md-12 banners-container">
+        <img class="image-banner" src="../assets/banner.svg" width="85%" alt="">
+      </div> -->
+    </div>
+    <div class="main-banner row col-md-12 mt-5 mb-5 laptop-padding-left">
+        <div class="col-md-7 col-12 banner-text">
+            <h2>Secure & Easy 24/7 Healthcare Management Platform</h2>
+            <p>Designed for Patients and Healthcare Professionals. Create, own, and personalize your healthcare anywhere in the world.</p>
+          <img src="../assets/googleplay.png" width="200px" class="m-0 p-0" alt="">
+          <p class="labels-search m-0 p-0">Subscribe to our news letters</p>
+          <input type="text" placeholder="enter your email id" class="search">
+        </div>
+        <div class="col-md-4 col-12">
+          <img src="../assets/ellipse.svg" class="ring" width="115%" alt="">
+          <img src="../assets/circle-tab.png" class="circle-1" alt="">
+          <img src="../assets/circle-tab.png" class="circle-2" alt="">
+          <img src="../assets/circle-tab.png" class="circle-3" alt="">
+          <img src="../assets/circle-tab.png" class="circle-4" alt="">
+          <img src="../assets/circle-tab.png" class="circle-5" alt="">
+          <img src="../assets/phone.png" class="phone" alt="">
+          <img src="../assets/stethoscope.png" class="stethoscope" alt="">
+          <img src="../assets/hospital.png" class="hospital" alt="">
+          <img src="../assets/drop.svg" class="drop" alt="">
+          <img src="../assets/drug.svg" class="drug" alt="">
+          <img src="../assets/ambulance.svg" class="ambulance" alt="">
+
+        </div>
+      </div>
+    <div class="features laptop-padding">
+      <div class="col-md-12 text-left m-0 p-0 pb-4">
+        <h2>Features</h2>
+      </div>
+      <div class="cards row col-md-12 justify-content-between m-0 p-0">
+        <div class="card col-md-40 col-12 mb-2 p-3">
+          <div class="row">
+            <div class="col-md-2 col-3">
+              <img src="../assets/time.png" width="50px" alt="">
+            </div>
+            <div class="col-md-10">
+              <p class="card-headers">24/7 secure real-time access to medical record</p>
+            </div>
+            <div class="col-md-12">
+              <p class="card-headers">24/7 secure real-time access to medical history including key vitals such as blood pressure and sugar level</p>
+            </div>
+          </div>
+        </div>
+        <div class="card col-md-40 mb-2 p-3">
+          <div class="row">
+            <div class="col-md-2 col-3">
+              <img src="../assets/doc.png" width="50px" alt="">
+            </div>
+            <div class="col-md-10">
+              <p class="card-headers">Connect with healthcare professionals near you</p>
+            </div>
+            <div class="col-md-12">
+              <p class="card-headers">
+                Book and connect online or in-person with healthcare professionals near you worldwide
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card col-md-40 mb-2 p-3">
+          <div class="row">
+            <div class="col-md-2 col-3">
+              <img src="../assets/prescription.png" width="50px" alt="">
+            </div>
+            <div class="col-md-10">
+              <p class="card-headers">Digital Prescription & Renewal</p>
+            </div>
+            <div class="col-md-12">
+              <p class="card-headers">
+                Operational efficiency through quick and secure digital prescription and renewal services so you are never left without your much-needed medication
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card col-md-40 mb-2 p-3">
+          <div class="row">
+            <div class="col-md-2 col-3">
+              <img src="../assets/mask.png" width="50px" alt="">
+            </div>
+            <div class="col-md-10">
+              <p class="card-headers">Remi Our Digital Doctor</p>
+            </div>
+            <div class="col-md-12">
+              <p class="card-headers">
+                Interact with our AI powered Doctor “Remi” for common medical conditions and hygiene recommendations
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card col-md-40 mb-2 p-3">
+          <div class="row">
+            <div class="col-md-2 col-3">
+              <img src="../assets/newss.png" width="50px" alt="">
+            </div>
+            <div class="col-md-10">
+              <p class="card-headers">Health News</p>
+            </div>
+            <div class="col-md-12">
+              <p class="card-headers">
+            Receive real-time update on medical breakthrough and information from healthcare professionals, institutions, and government agencies on emergency situations such as epidemic/pandemic              </p>
+            </div>
+          </div>
+        </div>
+        <div class="card col-md-40 mb-2 p-3">
+          <div class="row">
+            <div class="col-md-2 col-3">
+              <img src="../assets/family.png" width="50px" alt="">
+            </div>
+            <div class="col-md-10">
+              <p class="card-headers">Friends & Family Aftercare</p>
+            </div>
+            <div class="col-md-12">
+              <p class="card-headers">
+                Help manage the health of your friends and family; ensuring they have the necessary aftercare needed to live a normal life
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-12 text-center">
+          <img src="../assets/googleplay.png" width="200px" class="m-0 p-0" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="howitworks text-left">
+      <div class="laptop-padding-left pt-5">
+        <h2 style="color:#FFFFFF">OUR MISSION</h2>
+        <p style="color:#ffffff">Our mission is to provide a secure platform designed to allow</p>
+          <div class="mt-5">
+            <div class="col-md-12">
+            <img src="../assets/mission1.png" class="mission1" alt="">
+            </div>
+            <div class="mission1-text col-md-2 col-10">
+              <p class="mission-text">Real-time access to medical records</p>
+            </div>
+          </div>
+          <div>
+            <div class="col-md-12">
+            <img src="../assets/mission2.png" class="mission2" alt="">
+            </div>
+            <div class="mission2-text col-md-2 col-10 p-2">
+              <p class="mission-text">Digital prescription and home delivery</p>
+            </div>
+          </div>
+          <div>
+            <div class="col-md-12">
+            <img src="../assets/mission3.png" class="mission3" alt="">
+            </div>
+            <div class="mission3-text col-md-2 col-10">
+              <p class="mission-text">Easy interaction with our AI powered digital Doctor Remi</p>
+            </div>
+          </div>
+          <div>
+            <div class="col-md-12">
+            <img src="../assets/mission4.png" class="mission4" alt="">
+            </div>
+            <div class="mission4-text col-md-2 col-10">
+              <p class="mission-text">Booking and interacting with healthcare professionals online and in-person around the world in emergency and non-emergency situations</p>
+            </div>
+          </div>
+      </div>
+      
+    </div>
   </div>
 </template>
 
 <script>
+
+import navbar from '../components/navbar'
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  components:{
+    navbar
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@700;900&display=swap');
+
+@media only screen and (max-width: 800px) {
+  h2{
+    font-size: 20px !important;
+    line-height: 21px !important
+  }
+  p{
+    font-size: 15px !important;
+    line-height: 21px !important
+  }
+  .col-md-4{
+    width: 100%!important
+  }
+  .col-md-40{
+  flex: 0 0 100% !important;
+  max-width: 100% !important;
+  }
+  .banner-text{
+    margin-bottom: 3rem!important;
+  }
+  .laptop-padding-left{
+    padding-left: 1rem!important;
+  }
+  .laptop-padding{
+      padding: 0em 1rem!important;
+  }
+    img.circle-1 {
+    position: absolute;
+      top: 82%;
+      left: 47%;
+      width: 98px !important;
+  }
+  img.circle-2 {
+    position: absolute;
+      top: 65% !important;
+      left: 8% !important;
+      width: 98px !important;
+  }
+  img.circle-3 {
+      position: absolute;
+      top: 22%;
+      left: 0%;
+      width: 98px !important
+  }
+  img.circle-4 {
+        position: absolute;
+      top: -11%;
+      left: 80%;
+      width: 98px !important;
+  }
+  img.circle-5 {
+      position: absolute;
+      top: -11%;
+      left: 32%;
+      width: 98px !important;
+  }
+  img.phone {
+      position: absolute;
+      top: 17%;
+      left: 53%;
+      width: 145px !important;
+  }
+  img.stethoscope{
+    position: absolute;
+      top: -5%;
+      left: 39%;
+      width: 46px !important;
+  }
+  img.hospital{
+    position: absolute;
+    top: -5%;
+    left: 88%;
+    width: 46px !important;
+  }
+  img.drug{
+    position: absolute;
+    top: 30%;
+    left: 10%;
+    width: 46px !important;;
+  }
+  img.drop{
+    position: absolute;
+    top: 72% !important;
+    left: 15% !important;
+    width: 46px !important;;
+  }
+  img.ambulance{
+    position: absolute;
+    top: 93% !important;
+    left: 55% !important;
+    width: 46px !important;
+  }
+  .mission1{
+    width: 100% !important;
+    position: relative !important;
+    top: 0 !important;
+    left: 0 !important
+  }
+  .mission1-text{
+    top: 0 !important;
+    left: 0 !important
+  }
+  .mission2-text{
+    top: 0 !important;
+    left: 0 !important
+  }
+  .mission3-text{
+    top: 0 !important;
+    left: 0 !important
+  }
+  .mission4-text{
+    top: -14px !important;
+    left: 2px !important;
+  }
+  .mission-text{
+    color:white !important;
+    position: absolute !important;
+    top: -189px !important;
+    left: 38px !important
+  }
+  .mission2{
+    width: 100% !important;
+    position: relative !important;
+    top: 0 !important;
+    left: 0 !important
+  }
+  .mission3{
+    width: 100% !important;
+    position: relative !important;
+    top: 0 !important;
+    left: 0 !important
+  }
+  .mission4{
+    width: 100% !important;
+    position: relative !important;
+    top: 0 !important;
+    left: 0 !important
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+/* ends */
+.mission-text{
+  font-size: 15px;
+  color:white;
+   line-height: 21px;
+   font-weight: 400 !important
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.mission1{
+  position: absolute;
+  width: 18%
 }
-a {
-  color: #42b983;
+.mission1-text{
+  top: 88px;
+  left: 2%;
+  text-align: center;
+}
+.mission2 {
+  position: absolute;
+  width: 15%;
+  left: 16%;
+  top: 95px;
+}
+.mission2-text{
+  top: 159px;
+  left: 15%;
+  text-align: center;
+}
+.mission3 {
+  position: absolute;
+    width: 19%;
+    left: 30%;
+    top: -76px;
+}
+.mission3-text{
+  top: 0;
+  left: 31%;
+  text-align: center;
+}
+.mission4 {
+  position: absolute;
+    width: 26%;
+    left: 49%;
+    top: -125px;
+}
+.mission4-text{
+  top: -52px;
+    left: 53%;
+    text-align: center;
+}
+
+.howitworks{
+  overflow: hidden;
+  background-image: url('../assets/hiw-banner.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-origin: content-box;
+  min-height: 800px
+}
+.card-headers{
+  font-size: 14px;
+  line-height: 21px;
+  text-align: justify;
+  padding-top: 10px
+}
+.laptop-padding-left{
+  padding-left: 3rem;
+}
+.col-md-40{
+  flex: 0 0 33.333333%;
+    max-width: 32.333333%;
+}
+.card{
+  background-color: rgb(253,245,248);
+  box-shadow: 3px 3px 10px #00000026;
+border-radius: 10px;
+opacity: 1;
+border: none
+}
+.laptop-padding{
+  padding:1em 3rem;
+}
+.search{
+  border: 1px solid #FF73A7;
+  font-size: 15px;
+  padding-left: 10px;
+  width: 100% ;
+  height: 30px;
+  color: #FF73A7
+}
+.labels-search{
+  color:rgb(255,159,194);
+  font-size: 13px;
+}
+.overview-project{
+  overflow: hidden;
+  background-image: url('../assets/banner.svg');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-origin: content-box;
+  background-position: 395px -107px !important;
+}
+.ring {
+    margin-top: 0%;
+    margin-left: 15%;
+}
+h2{
+  font-family: 'Raleway', sans-serif;
+  font-size: 40px;
+  color:#343434;
+  line-height: 48px;
+  font-weight: 900
+}
+p{
+  font-family: 'Raleway', sans-serif;
+  font-size: 27px;
+  color:#343434;
+  font-weight: 700;
+  line-height: 41px
+}
+.banner-text{
+  text-align: left;
+}
+.top-container{
+  overflow: hidden
+}
+.image-banner{
+  /* position: absolute */
+}
+.banners-container{
+    margin-top: -154px;
+    margin-left: 200px;
+    z-index: -1;
+    position: fixed
+}
+img.circle-1 {
+   position: absolute;
+    top: 82%;
+    left: 47%;
+    width: 150px;
+}
+img.circle-2 {
+  position: absolute;
+    top: 67%;
+    left: 0%;
+    width: 150px;
+}
+img.circle-3 {
+    position: absolute;
+    top: 22%;
+    left: 0%;
+    width: 150px
+}
+img.circle-4 {
+      position: absolute;
+    top: -11%;
+    left: 80%;
+    width: 140px;
+}
+img.circle-5 {
+    position: absolute;
+    top: -11%;
+    left: 32%;
+    width: 140px;
+}
+img.phone {
+    position: absolute;
+    top: 17%;
+    left: 51%;
+    width: 191px;
+}
+img.stethoscope{
+  position: absolute;
+    top: -5%;
+    left: 39%;
+    width: 75px;
+}
+img.hospital{
+  position: absolute;
+    top: -5%;
+    left: 88%;
+    width: 75px;
+}
+img.drug{
+  position: absolute;
+    top: 30%;
+    left: 10%;
+    width: 75px;;
+}
+img.drop{
+  position: absolute;
+    top: 74%;
+    left: 8%;
+    width: 75px;;
+}
+img.ambulance{
+  position: absolute;
+    top: 95%;
+    left: 56%;
+    width: 75px;
 }
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="navigation-container mt-2">
+        <nav class="navbar d-flex justify-content-between navbar-expand-lg">
+           <div class="col-6 col-md-2 text-left m-0 p-0">
+                <img src="../assets/logo.png" alt="" style="max-height: 50px;">
+           </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                   <i class="fa fa-bars" aria-hidden="true"></i>
+                </button>
+            <div class="collapse col-md-10 pull-right navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link px-3" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="#">How it works?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="#">Our Mission.</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="#">Why us?</a>
+                </li>
+                 <li class="nav-item">
+                    <a class="nav-link px-3" href="#">Meet our team.</a>
+                </li>
+                <!-- <li class="nav-item dropdown">
+                    <a class="nav-link px-3 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                </li> -->
+                <li class="nav-item">
+                    <button class="btn med-button">Contact</button>
+                </li>
+                </ul>
+                <!-- <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form> -->
+            </div>
+        </nav>
+    </div>
+</template>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@600;700;900&display=swap');
+
+.nav-item{
+    font-family: 'Raleway', sans-serif;
+    font-weight: 600;
+   
+}
+.nav-item a{
+     color: #343434 !important;
+}
+.med-button{
+    background-color: white;
+    color:rgb(66,180,231)
+}
+</style>
